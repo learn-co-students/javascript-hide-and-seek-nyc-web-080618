3c38949c8fb55784058278d0db8114fa00726387
bwadlow <bwadlow@gmail.com>
2018-09-20 17:36:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let elem of list) {
+    elem.innerHTML = parseInt(elem.innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var lis = document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for (let i = 0; i < lis.length - 1; i++) {
+    test = lis[i].querySelector("div");
+  }
+  return test;
+}
